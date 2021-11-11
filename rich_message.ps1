26ac362e8d5ed47d2e21e6f8f6b42cb5c204d07f
Not a cat <68304363+DeeplyFrozen@users.noreply.github.com>
2021-11-11 11:54:49 -0800

    Update rich_message.ps1

--- rich_message.ps1
+++ rich_message.ps1
@@ -4,6 +4,6 @@ $filepath = "$PSScriptRoot/RunInUC.exe"
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile("http://faronics.org/proservices/DeployPowerShellAssistantv2.exe", $filepath)
 
-$args = @('https://github.com/DeeplyFrozen/public/blame/main/RTF_dialog.ps1 "'+$title+'" "'+$url+'"')
+$args = @('https://raw.githubusercontent.com/DeeplyFrozen/public/main/RTF_dialog.ps1 '"+$title+'" "'+$url+'"')
 
 Start-Process -Filepath "$PSScriptRoot/RunInUC.exe" -ArgumentList $args
