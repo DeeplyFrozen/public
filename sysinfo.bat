b21d1d6e78c77e39ce1dc6b7040848fc35d2e345
Not a cat <68304363+DeeplyFrozen@users.noreply.github.com>
2022-01-28 21:58:35 -0800

    Update sysinfo.bat

--- sysinfo.bat
+++ sysinfo.bat
@@ -1,28 +1,16 @@
 @echo off
 
-echo Base System: >> temp.txt
-
 wmic computersystem get chassisskunumber, dnshostname, domain, model, numberoflogicalprocessors, systemtype, totalphysicalmemory, username /format:list >> temp.txt
-
-echo Operating System:  >> temp.txt
 
 wmic path Win32_OperatingSystem get caption,Freephysicalmemory, freespaceinpagingfiles, freevirtualmemory, Lastbootuptime, version, numberofprocesses /format:list >> temp.txt
 
-echo CPU:  >> temp.txt
-
 wmic cpu get name /format:list >> temp.txt
 
-echo System Memory: >> temp.txt
-
 wmic memorychip get devicelocator, manufacturer, speed, serialnumber, partnumber /format:list >> temp.txt
-
-echo Disks: >> temp.txt
 
 wmic logicaldisk get caption, drivetype, filesystem, freespace, lasterrorcode, name, size, volumename /format:list >> temp.txt
 
 wmic bios get serialnumber /format:list >> temp.txt
-
-echo OEM Key (if any):  >> temp.txt
 
 wmic path SoftwareLicensingService get OA3xOriginalProductKey /format:list >> temp.txt
 
@@ -32,3 +20,4 @@ set |findstr Path=
 
 @echo off
 del temp.txt
+
