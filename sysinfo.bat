be2e97eb6776c63f86680f8a6c2b148e0bc33b67
Not a cat <68304363+DeeplyFrozen@users.noreply.github.com>
2022-01-28 20:42:54 -0800

    Update sysinfo.bat

--- sysinfo.bat
+++ sysinfo.bat
@@ -1,11 +1,13 @@
-wmic path Win32_OperatingSystem get caption,buildnumber,Freephysicalmemory, freespaceinpagingfiles, freevirtualmemory, Lastbootuptime, serialnumber, version, numberofprocesses
+wmic path Win32_OperatingSystem get caption,buildnumber,Freephysicalmemory, freespaceinpagingfiles, freevirtualmemory, Lastbootuptime, serialnumber, version, numberofprocesses  >> temp.txt
 
-wmic cpu get name
+wmic cpu get name >> temp.txt
 
-wmic memorychip get devicelocator, manufacturer, speed, serialnumber, partnumber
+wmic memorychip get devicelocator, manufacturer, speed, serialnumber, partnumber >> temp.txt
 
-wmic bios get serialnumber
+wmic bios get serialnumber >> temp.txt
 
-wmic path SoftwareLicensingService get OA3xOriginalProductKey
+wmic path SoftwareLicensingService get OA3xOriginalProductKey >> temp.txt
 
-set |findstr Path=
+set |findstr Path= >> temp.txt
+
+type temp.txt
