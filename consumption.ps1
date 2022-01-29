9a352214a6fb97ce3924496ae641f5783f5fe15f
Not a cat <68304363+DeeplyFrozen@users.noreply.github.com>
2022-01-29 14:41:39 -0800

    Update consumption.ps1

--- consumption.ps1
+++ consumption.ps1
@@ -2,7 +2,7 @@ write-host "Top 10 programs by memory usage:"
 Get-Process | sort-object -Descending WS | select-object -first 10
 write-host "Top 10 programs by CPU usage:"
 Get-Process | sort-object -Descending CPU | select-object -first 10
-write-host "10 newest System log errors:"
-get-eventlog -logname System -newest 10 -entrytype error
-write-host "10 newest Application log errors:"
-get-eventlog -logname Application -newest 10 -entrytype error
+write-host "5 newest System log errors:"
+get-eventlog -logname System -newest 5 -entrytype error
+write-host "5 newest Application log errors:"
+get-eventlog -logname Application -newest 5 -entrytype error
