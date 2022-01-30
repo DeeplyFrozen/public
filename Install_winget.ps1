7c7cbaf4bde63ad27e6dd81d35e437606d997a2d
Not a cat <68304363+DeeplyFrozen@users.noreply.github.com>
2022-01-29 20:15:01 -0800

    Update Install_winget.ps1

--- Install_winget.ps1
+++ Install_winget.ps1
@@ -1,3 +1,1 @@
-Set-ExecutionPolicy -ExecutionPolicy Bypass -scope LocalMachine
 Get-AppPackage *Microsoft.DesktopAppInstaller*|select Name,PackageFullName
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -scope LocalMachine
