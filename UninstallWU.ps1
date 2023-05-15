9e155839d0eea5a2eb8277a1e9b52d6a172b2ed4
Not a cat <68304363+DeeplyFrozen@users.noreply.github.com>
2023-05-15 16:16:20 -0700

    Update UninstallWU.ps1

--- UninstallWU.ps1
+++ UninstallWU.ps1
@@ -1,3 +1,6 @@
+#dism /online /get-packages | findstr "Package_for"#
+#$update = $SearchUpdates.replace("Package Identity : ", "") | findstr "KBXXXXXXX"#
+
 param($update)
 DISM.exe /Online /Remove-Package /PackageName:$update /quiet /norestart
 write-output($update)
