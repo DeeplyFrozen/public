4c54f9e52e2b6e1b357ef1cf2fe6921ec1a8c55d
Not a cat <68304363+DeeplyFrozen@users.noreply.github.com>
2024-04-29 14:38:16 -0700

    Update Install_winget.ps1

--- Install_winget.ps1
+++ Install_winget.ps1
@@ -1,2 +1,2 @@
-Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.7.11132/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "c:\windows\temp\WinGet.msixbundle"
-Add-AppxPackage "c:\windows\temp\WinGet.msixbundle"
+Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.7.11132/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "c:\windows\temp\WinGet.msixbundle" -verbose
+Add-AppxPackage "c:\windows\temp\WinGet.msixbundle" -verbose
