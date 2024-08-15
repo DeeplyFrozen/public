85082482ef88d84002d521a90a3d47a363903263
Not a cat <68304363+DeeplyFrozen@users.noreply.github.com>
2024-08-15 12:48:05 -0700

    Update set_autologin.bat
    whoops

--- set_autologin.bat
+++ set_autologin.bat
@@ -1,3 +1,3 @@
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogin /t REG_SZ /d 1
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUsername /t REG_SZ /d %1
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d %2
